95be3ed8d0ee08206afaa108e930e53263729e0a
Luiz Gustavo <luiz.gustavo@automatuslab.com>
2024-07-14 12:47:55 -0300

    first

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/main.css'
+import 'primeicons/primeicons.css'
+import router from '@/router/index'
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App)
+.use(router)
+.use(Toast)
+.mount('#app')
